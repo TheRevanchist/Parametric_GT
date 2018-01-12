48d18d5cada913954fbfd988b4e57e906c29f5d0
aretor <torci@live.it>
2018-01-12 11:07:56 +0100

    add gen_gtg_dataset function
    gen_gtg_dataset takes in input a well-formatted file containing
    filenames and the new folders to which they have been assigned after
    having performed the gtg and generates a new folder tree arranging those
    files in the new folders

--- split_dataset.py
+++ split_dataset.py
@@ -8,15 +8,15 @@ root = 'Datasets'
 def split(dataset_name, tr_perc, n=1):
     source = os.path.join(root, dataset_name)
     folders = os.listdir(source)
-    for i in range(n):
 
+    for i in range(n):
         for folder in os.listdir(source):
             source_folder = os.path.join(source, folder)
             train_folder = os.path.join(root, 'train_' + str(n), folder)
             test_folder = os.path.join(root, 'test_' + str(n), folder)
 
-            os.makedirs(os.path.join(root, 'train_' + str(n), folder))
-            os.makedirs(os.path.join(root, 'test_' + str(n), folder))
+            os.makedirs(train_folder)
+            os.makedirs(test_folder)
 
             files = os.listdir(source_folder)
             random.shuffle(files)
@@ -30,4 +30,17 @@ def split(dataset_name, tr_perc, n=1):
                 shutil.copy(os.path.join(source_folder, file), test_folder)
 
 
-split('caltech', 0.7, n=1)
+def gen_gtg_dataset(dataset_name, data_fname, lab_perc):
+    source = os.path.join(root, dataset_name)
+
+    with open(data_fname, 'r') as f:
+        for line in f:
+            fname, lab = line.split(sep=' ')
+            dst_pname = os.path.join(root, 'train_gtg_' + str(lab_perc), lab)
+            os.makedirs(dst_pname)
+            shutil.copy(os.path.join(source, fname), dst_pname)
+
+
+if __name__ == '__main__':
+    # split('caltech', 0.7, n=1)
+    gen_gtg_dataset('caltech', '<insert name of lab file here>', 0.1)
