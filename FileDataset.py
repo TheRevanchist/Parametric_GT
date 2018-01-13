b5dab8563b74381bb205e4bda73ee39f8b064abe
aretor <torci@live.it>
2018-01-13 11:55:35 +0100

    add FileDataset.py
    auxiliary classes to generate datasets by passing a filename.

---
+++ FileDataset.py
@@ -0,0 +1,30 @@
+import os
+from skimage import io
+
+import torch.utils.data
+
+
+class FileDataset(torch.utils.data.Dataset):
+    def __init__(self, fname, root_dir, transform):
+        with open(fname, 'r') as f:
+            data = list(map(lambda line: line.split(' '), f))
+            self.fnames = list(map(lambda item: os.path.join(root_dir, item[0]), data))
+            self.labs = list(map(lambda item: item[1], data))
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        fname = os.path.join(self.root_dir, self.fnames[idx][0])
+        img = io.imread(fname)
+        if self.transform:
+            img = self.transform(img)
+        return img, self.labs[idx], self.fnames[idx]
+
+    def __len__(self):
+        return len(self.data)
+
+
+class CaltechFileDataset(FileDataset):
+    def __init__(self, fname, root_dir, transform):
+        super(CaltechFileDataset, self).__init__(fname, root_dir, transform)
+        self.labs = list(map(lambda item: int(item[:3]), self.labs))
