35a9af7643ce88f94f9e0cd105fb8694f3361a40
TheRevanchist <DarthRevan19>
2018-01-14 10:15:58 +0100

    add feature extraction for the test dataset

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from net import evaluate
 def main():
     root = '.'
     current_dataset = 'caltech'
-    out_dir = os.path.join(root, 'out', current_dataset, 'feature_data_test')
+    out_dir_tr = os.path.join(root, 'out', current_dataset, 'feature_data')
+    out_dir_test = os.path.join(root, 'out', current_dataset, 'feature_data_test')
     batch_size = 1
 
     nets_and_features = create_dict_nets_and_features()
@@ -25,20 +26,30 @@ def main():
         for i, net_type in enumerate(list_of_net_names):
             net, feature_size = fe.get_net_info(net_type.split("_")[0], number_of_classes, nets_and_features)
 
-            train_loader = prepare_loader_val(dataset_test, stats, batch_size)
+            train_loader = prepare_loader_val(dataset_train, stats, batch_size)
+            test_loader = prepare_loader_val(dataset_test, stats, batch_size)
+
             # if net is densenet
             if net_type[:3] == 'den':
-                fc7_features_tr, labels, net, fnames = fe.extract_features_train(net, feature_size, dataset_size_test,
+                fc7_features_tr, labels_tr, net_tr, fnames_tr = fe.extract_features_train(net, feature_size, dataset_size_train,
                                                                                  train_loader, dense=1)
+                fc7_features_test, labels_test, net_test, fnames_test = fe.extract_features_train(net, feature_size, dataset_size_test,
+                                                                                 test_loader, dense=1)
             # if net is resnet
             else:
-                fc7_features_tr, labels, net, fnames = fe.extract_features_train(net, feature_size, dataset_size_test,
+                fc7_features_tr, labels_tr, net_tr, fnames_tr = fe.extract_features_train(net, feature_size, dataset_size_train,
+                                                                                 train_loader, dense=0)
+                fc7_features_test, labels_test, net_test, fnames_test = fe.extract_features_train(net, feature_size, dataset_size_test,
                                                                                  train_loader, dense=0)
 
             # store the name of the net, the dataset on which we are going to use it, and the testing accuracy
-            net_info = [net_type.split("_")[0], labels, fc7_features_tr, fnames]
-            with open(os.path.join(out_dir, net_type.split("_")[0] + '_' + str(j) + '.pickle'), 'wb') as f:
-                pickle.dump(net_info, f, pickle.HIGHEST_PROTOCOL)
+            net_info_tr = [net_type.split("_")[0], labels_tr, fc7_features_tr, fnames_tr]
+            with open(os.path.join(out_dir_tr, net_type.split("_")[0] + '_' + str(j) + '.pickle'), 'wb') as f:
+                pickle.dump(net_info_tr, f, pickle.HIGHEST_PROTOCOL)
+
+            net_info_test = [net_type.split("_")[0], labels_test, fc7_features_test, fnames_test]
+            with open(os.path.join(out_dir_test, net_type.split("_")[0] + '_' + str(j) + '.pickle'), 'wb') as f:
+                pickle.dump(net_info_test, f, pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
