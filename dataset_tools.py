007a1f963cbaf61bc9f51ca88a193cbf74abb7f0
aretor <torci@live.it>
2018-01-15 15:12:20 +0100

    add function to generate dataset files
    make_source_file generate a text file containing the path to all the
    observations in a dataset.
    
    split_source_file splits the previously generated file to obtain a
    train/test split.

--- dataset_tools.py
+++ dataset_tools.py
@@ -5,7 +5,33 @@ import random
 root = 'Datasets'
 
 
-def split(dataset_name, tr_perc, n=5):
+def make_source_file(dataset_name):
+    source = os.path.expanduser(dataset_name)
+    if not os._exists('dataset_files'):
+        os.makedirs('dataset_files')
+
+    with open('dataset_files/source_file.txt', 'w') as f:
+        for class_dir in os.listdir(source):
+            for fname in os.listdir(os.path.join(source, class_dir)):
+                f.write(os.path.join(class_dir, fname) + '\n')
+
+
+def split_source_file(source_file, tr_perc=0.7, n=1):
+    with open(source_file, 'r') as f:
+        f = list(f)
+        split = int(tr_perc * len(f))
+        for i in xrange(n):
+            random.shuffle(f)
+            with open('train_' + str(i) + '.txt', 'w') as train:
+                for line in f[:split]:
+                    train.write(line)
+
+            with open('test_' + str(i) + '.txt', 'w') as test:
+                for line in f[split:]:
+                    test.write(line)
+
+
+def split(dataset_name, tr_perc, n=1):
     source = os.path.join(root, dataset_name)
 
     for i in range(n):
@@ -48,6 +74,8 @@ def gen_gtg_dataset(dataset_name, data_fname, lab_perc):
 
 
 if __name__ == '__main__':
-    split('caltech/caltech_src', 0.7, n=5)
     random.seed(2718)
+    # make_source_file('Datasets/caltech/caltech_src')
+    split_source_file('dataset_files/source_file.txt', tr_perc=0.7, n=5)
+    # split('caltech/caltech_src', 0.7, n=5)
     # gen_gtg_dataset('caltech/train', 'only_labelled.txt', 0.1)
